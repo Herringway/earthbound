c2a7320887b9d43d7beca65d642af63f2cf23aa9
Cameron Ross <elpenguino@gmail.com>
2021-10-19 18:35:09 -0300

    add inflictSunstrokeCheck

--- source/earthbound/bank02.d
+++ source/earthbound/bank02.d
@@ -1,6 +1,31 @@
 module earthbound.bank02;
 
-void inflictSunstrokeCheck() {}
+import earthbound.bank00;
+import earthbound.globals;
+
+void inflictSunstrokeCheck() {
+	if (OverworldStatusSuppression) {
+		return;
+	}
+	if ((gameState.troddenTileType & 0xC) != 4) {
+		return;
+	}
+	for (int i = 0 ; i < 6; i++) {
+		if (gameState.unknown96[i] == 0) {
+			return;
+		}
+		if (gameState.unknown96[i] > 4) {
+			return;
+		}
+		Unknown7E4DC6 = ChosenFourPtrs[gameState.playerControlledPartyMembers[i]];
+		if (Unknown7E4DC6.afflictions[0] != 0 && Unknown7E4DC6.afflictions[0] == 7) {
+			continue;
+		}
+		if (((30 - Unknown7E4DC6.guts > 0) ? (30 - Unknown7E4DC6.guts) : 1) / 100 == rand()) {
+			Unknown7E4DC6.afflictions[0] = 6;
+		}
+	}
+}
 
 immutable ubyte[] C200B9 = [0xF8, 0xFF, 0x00, 0x00, 0x07, 0x00, 0xF8, 0xFF, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x00, 0x07, 0x00, 0x07, 0x00, 0x1E, 0x00, 0x33, 0x00, 0x1E, 0x00, 0x33, 0x00];
 
