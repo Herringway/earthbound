5d178a9bfffb3fb836cd6c5fad384a2c779131ed
Cooper Harasyn <charasyn@outlook.com>
2022-01-18 16:43:55 -0800

    Add basic sdl renderer

--- source/main.d
+++ source/main.d
@@ -1,2 +1,180 @@
+import std.stdio;
+import std.string : fromStringz, format;
+import core.thread : Fiber;
 
-void main() {}
+import bindbc.loader;
+import bindbc.sdl;
+
+import earthbound.bank00 : start, NMI;
+import earthbound.hardware : JOYPAD_1_DATA, JOYPAD_2_DATA;
+
+import snesdrawframe;
+import snesdrawframedata;
+
+private enum WindowScale = 1;
+
+void saveGraphicsStateToFile(string filename) {
+    File file = File(filename, "wb");
+    file.rawWrite([g_frameData]);
+}
+
+void setJoypad() {
+    auto state = SDL_GetKeyboardState(null);
+    ushort joy1 = 0;
+    if (state[SDL_Scancode.SDL_SCANCODE_S]) joy1 |= 0x8000; // B
+    if (state[SDL_Scancode.SDL_SCANCODE_A]) joy1 |= 0x4000; // Y
+    if (state[SDL_Scancode.SDL_SCANCODE_X]) joy1 |= 0x2000; // Select
+    if (state[SDL_Scancode.SDL_SCANCODE_Z]) joy1 |= 0x1000; // Start
+    if (state[SDL_Scancode.SDL_SCANCODE_UP]) joy1 |= 0x0800; // Up
+    if (state[SDL_Scancode.SDL_SCANCODE_DOWN]) joy1 |= 0x0400; // Down
+    if (state[SDL_Scancode.SDL_SCANCODE_LEFT]) joy1 |= 0x0200; // Left
+    if (state[SDL_Scancode.SDL_SCANCODE_RIGHT]) joy1 |= 0x0100; // Right
+    if (state[SDL_Scancode.SDL_SCANCODE_D]) joy1 |= 0x0080; // A
+    if (state[SDL_Scancode.SDL_SCANCODE_W]) joy1 |= 0x0040; // X
+    if (state[SDL_Scancode.SDL_SCANCODE_Q]) joy1 |= 0x0020; // L
+    if (state[SDL_Scancode.SDL_SCANCODE_E]) joy1 |= 0x0010; // R
+    JOYPAD_1_DATA = joy1;
+    JOYPAD_2_DATA = 0;
+}
+
+void main() {
+    if(!loadSnesDrawFrame()) {
+        writeln("Can't load SnesDrawFrame!");
+        return;
+    }
+    if(loadSDL() < sdlSupport) {
+        writeln("Can't load SDL!");
+        return;
+    }
+    if(!initSnesDrawFrame()) {
+        writeln("Error initializing SnesDrawFrame!");
+        return;
+    }
+
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        writeln("Error initializing SDL: ", fromStringz(SDL_GetError()));
+        return;
+    }
+    scope(exit) {
+      SDL_Quit();
+    }
+
+    const windowFlags = SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE;
+    SDL_Window* appWin = SDL_CreateWindow(
+        "SDL Window",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        ImgW * WindowScale,
+        ImgH * WindowScale,
+        windowFlags
+    );
+    if(appWin is null) {
+        writeln("Error creating SDL window: ", fromStringz(SDL_GetError()));
+        return;
+    }
+    scope(exit) {
+        // Close and destroy the window
+        if (appWin !is null) {
+            SDL_DestroyWindow(appWin);
+        }
+    }
+
+    const rendererFlags = SDL_RENDERER_ACCELERATED;
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        appWin, -1, rendererFlags
+    );
+    if(renderer is null) {
+        writeln("Error creating SDL renderer: ", fromStringz(SDL_GetError()));
+        return;
+    }
+    scope(exit) {
+        // Close and destroy the renderer
+        if (renderer !is null) {
+            SDL_DestroyRenderer(renderer);
+        }
+    }
+
+    SDL_Texture* drawTexture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_RGB555,
+        SDL_TEXTUREACCESS_STREAMING,
+        ImgW,
+        ImgH
+    );
+    if(drawTexture is null) {
+        writeln("Error creating SDL texture: ", fromStringz(SDL_GetError()));
+        return;
+    }
+    scope(exit) {
+        // Close and destroy the texture
+        if (drawTexture !is null) {
+            SDL_DestroyTexture(drawTexture);
+        }
+    }
+
+    bool run = true, dumpVram = false, pause = false, step = false;
+    int dumpVramCount = 0;
+
+    int lastTime;
+    auto game = new Fiber(&start);
+    while(run) {
+        step = !pause;
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                run = false;
+            }
+            if (event.type == SDL_EventType.SDL_KEYDOWN) {
+                switch (event.key.keysym.scancode) {
+                    case SDL_Scancode.SDL_SCANCODE_0:
+                        dumpVram = true;
+                        break;
+                    case SDL_Scancode.SDL_SCANCODE_P:
+                        pause = !pause;
+                        break;
+                    case SDL_Scancode.SDL_SCANCODE_BACKSLASH:
+                        step = true;
+                        break;
+                    default: break;
+                }
+            }
+        }
+        auto kbdstate = SDL_GetKeyboardState(null);
+
+
+        lastTime = SDL_GetTicks();
+
+        ushort* drawBuffer;
+        int drawPitch;
+        SDL_LockTexture(drawTexture, null, cast(void**)&drawBuffer, &drawPitch);
+
+        if (step) {
+            setJoypad();
+            Throwable t = game.call(Fiber.Rethrow.no);
+            if(t) {
+                writeln(t);
+                throw t;
+            }
+            NMI();
+            copyGlobalsToFrameData();
+        }
+        drawFrame(drawBuffer, drawPitch, &g_frameData);
+        if (dumpVram) {
+            saveGraphicsStateToFile(format!"gfxstate%03d.bin"(dumpVramCount));
+            dumpVram = false;
+            dumpVramCount += 1;
+        }
+
+        SDL_UnlockTexture(drawTexture);
+
+        SDL_SetRenderDrawColor(renderer, 120, 140, 230, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, drawTexture, null, null);
+        SDL_RenderPresent(renderer);
+
+        int drawTime = SDL_GetTicks() - lastTime;
+        if(!(kbdstate[SDL_Scancode.SDL_SCANCODE_GRAVE]) && drawTime < 16) {
+            SDL_Delay(16 - drawTime);
+        }
+    }
+}
---
+++ source/sfcdma.d
@@ -0,0 +1,68 @@
+module sfcdma;
+import snesdrawframedata;
+import earthbound.hardware;
+
+void dmaCopy(byte * src, byte * dst, byte * wrapAt, byte * wrapTo,
+             int count, int transferSize, int srcAdjust, int dstAdjust) {
+    if(count == 0) count = 0x10000;
+    for(int i = 0; i < count;) {
+        for(int j = 0; i < count && j < transferSize; i += 1, j += 1) {
+            if (dst >= wrapAt) dst = wrapTo;
+            *dst = *src;
+            src++;
+            dst++;
+        }
+        src += srcAdjust;
+        dst += dstAdjust;
+    }
+}
+
+void handleDma() {
+    auto channels = MDMAEN;
+    for(auto i = 0; i < 8; i += 1) {
+        if (((channels >> i) & 1) == 0) continue;
+        auto dmap = DMAChannels[i].DMAP;
+        auto bbad = DMAChannels[i].BBAD;
+        assert((dmap & 0x80) == 0); // Can't go from B bus to A bus
+        assert((dmap & 0x10) == 0); // Can't decrement pointer
+        byte* dest, wrapAt, wrapTo;
+        int transferSize = 1, srcAdjust = 0, dstAdjust = 0;
+        if (bbad == 0x04) {
+            // Dest is OAM
+            assert((dmap & 0x07) == 0);
+            wrapTo = cast(byte *)(&g_frameData.oam1);
+            dest = wrapTo + ((OAMADDL | (OAMADDH & 3) << 8) << 1);
+            wrapAt = wrapTo + 0x220;
+        } else if (bbad == 0x18 || bbad == 0x19) {
+            // Dest is VRAM
+            auto hibyte = bbad == 0x19;
+            // Ensure we're only doing single byte to $2119
+            assert(!hibyte || (dmap & 0x07) == 0);
+            // Set transfer size
+            // Ensure we're either copying one or two bytes
+            assert((dmap & 0x07) <= 1);
+            if ((dmap & 0x07) == 1) {
+                transferSize = 2;
+                dstAdjust = 0;
+            } else {
+                transferSize = 1;
+                dstAdjust = 1; // skip byte when copying
+            }
+            wrapTo = cast(byte *)(&g_frameData.vram);
+            dest = wrapTo + ((VMADDL | VMADDH << 8) << 1 + (hibyte ? 1 : 0));
+            wrapAt = wrapTo + 0x10000;
+        } else if (bbad == 0x22) {
+            // Dest is CGRAM
+            assert((dmap & 0x07) == 0);
+            wrapTo = cast(byte *)(&g_frameData.cgram);
+            dest = wrapTo + (CGADD << 1);
+            wrapAt = wrapTo + 0x200;
+        }
+        // If the "Fixed Transfer" bit is set, transfer same data repeatedly
+        if ((dmap & 0x08) != 0) srcAdjust = -transferSize;
+        // Perform actual copy
+        dmaCopy(cast(byte *)DMAChannels[i].A1T, dest, wrapAt, wrapTo,
+                DMAChannels[i].DAS, transferSize, srcAdjust, dstAdjust);
+    }
+    MDMAEN = 0;
+}
