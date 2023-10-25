f8a0ca2a7a6e840de7a58e2604a833774ceeefbd
Cameron Ross <elpenguino@gmail.com>
2023-10-25 19:10:30 -0300

    fix bubble monkey movement crash

--- source/earthbound/bank2F.d
+++ source/earthbound/bank2F.d
@@ -150,7 +150,7 @@ void unknownEF031E() {
 	switch (unknown7E9F33) {
 		case 0:
 		case 2:
-			currentPartyMemberTick.positionIndex = unknownC03EC3(entityScriptVar0Table[currentEntitySlot], 0xC, partyCharacters[entityScriptVar1Table[currentEntitySlot]].positionIndex, 2);
+			currentPartyMemberTick.positionIndex = unknownC03EC3(entityScriptVar0Table[currentEntitySlot], 0xC, partyCharacters[entityScriptVar1Table[currentEntitySlot]].positionIndex, 2) & 0xFF;
 			if ((x16 == currentPartyMemberTick.positionIndex) || (x16 + 1 == currentPartyMemberTick.positionIndex)) {
 				doPartyMovementFrame(entityScriptVar0Table[currentEntitySlot], playerPositionBuffer[partyCharacters[entityScriptVar1Table[currentEntitySlot]].positionIndex].walkingStyle, currentEntitySlot);
 				if (gameState.leaderHasMoved == 0) {
