a676eecc1ae352c0c9d326e03c471dec6ea39f75
Cameron Ross <elpenguino@gmail.com>
2024-05-08 19:24:52 -0300

    more prefillKeyboardInput tests

--- source/earthbound/bank04.d
+++ source/earthbound/bank04.d
@@ -2107,6 +2107,15 @@ unittest {
 	assert(keyboardInputCharacterOffsets == [40, 73, 32, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 	assert(keyboardInputCharacterWidths == [6, 2, 6, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 	assert(keyboardInputCharacters == [120, 153, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+	prefillKeyboardInput(ebStringz("Hello").ptr, 5);
+	assert(keyboardInputCharacterOffsets == [40, 69, 76, 76, 79, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+	assert(keyboardInputCharacterWidths == [6, 5, 2, 2, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+	assert(keyboardInputCharacters == [120, 149, 156, 156, 159, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+	prefillKeyboardInput(ebStringz("").ptr, 5); // not normally possible, but here for completeness
+	keyboardInputCharacters[] = 0; // buffer doesn't get cleared automatically
+	assert(keyboardInputCharacterOffsets == [32, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+	assert(keyboardInputCharacterWidths == [6, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+	assert(keyboardInputCharacters == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 }
 
 /// $C441B7
